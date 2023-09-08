a42bcb936e646f86806979ed5ad49c899a3e0a84
Daniel Malmqvist <dnlmalmqvist@gmail.com>
2023-09-08 15:39:17 +0900

    New 7 kyuu

---
+++ javascript/7Kyu/Area-of-Circle.js
@@ -0,0 +1,19 @@
+var circleArea = function (radius) {
+  return radius > 0 ? +(radius * radius * Math.PI).toFixed(2) : false
+}
+
+/*
+DESCRIPTION:
+Complete the function which will return the area of a circle with the given radius.
+
+Round the returned number to two decimal places. If the radius is not positive or not a number, return false.
+
+Example:
+
+circleArea(-1485.86);    //returns false
+circleArea(0);           //returns false
+circleArea(43.2673);     //returns 5881.25
+circleArea(68);          //returns 14526.72
+circleArea("number");    //returns false
+
+*/
